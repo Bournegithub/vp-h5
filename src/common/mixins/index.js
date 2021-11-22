da661dc71377ce16dc6d14bd53e87ebfe9923d9c
bourne.yang <bourne.yang@analyticservice.net>
2021-11-22 10:58:10 +0800

    自定义图标

--- src/common/mixins/index.js
+++ src/common/mixins/index.js
@@ -1,5 +1,5 @@
 // filter mixins
-// vue 3.x中删除了过滤器，推荐使用computed来实现.为了目录结构清晰，以及功能函数的单一性，将需要使用的全局过滤器作用的函数集中放置在flter文件夹内,引入到混入对象
+// vue 3.x中删除了过滤器，推荐使用computed来实现.为了目录结构清晰，以及utils文件夹内功能函数的单一性，将需要使用的全局过滤器作用的计算属性集中放置在flter文件夹内,如需调用utils内功能函数也要保证utils文件夹内功能函数的单一性,引入到混入对象
 import globalFilters from '@filter/index.js';
 
 const globalMixin = {
--- src/main.js
+++ src/main.js
@@ -12,10 +12,10 @@ import '@assets/style/main.less';
 
 const app = createApp(App);
 app.config.globalProperties.$i18n = i18n; // i18n挂载到全局
-console.log('app.config', app.config);
+// console.log('app.config', app.config);
 app.use(i18n);
 vantImport(app); // 为避免main.js过于臃肿，将按需引入的组件放置在utils/vantImport
 app.use(router);
 app.use(store);
-app.mixin(globalMixin);
+app.mixin(globalMixin); // 全局混入， 将作为全局过滤器使用的计算属性和全局函数混入
 app.mount('#app');
