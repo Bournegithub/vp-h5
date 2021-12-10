3970c6492dd12837ec326faabd10ab1384e4834d
bourne.yang <bourne.yang@analyticservice.net>
2021-12-10 16:29:23 +0800

    navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -12,39 +12,36 @@
 </template>
 
 <script>
-// import { computed } from 'vue';
-// import { useRoute } from 'vue-router';
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
 
 export default {
-  // setup () {
-  //   const route = useRoute();
-  //   const pathName = computed(() =>route.name);
-  //   // const router = useRouter();
-  //   // console.log('router', router);
-  //   // const onClickLeft = () => history.back();
-  //   // const onClickLeft = () => router.go(-1);
-  //   // const onClickRight = () => Toast('按钮');
-  //   return {
-  //     pathName,
-  //     // onClickLeft,
-  //     // onClickRight,
-  //   };
-  // },
-  data () {
+  setup () {
+    const route = useRoute();
+    const pathName = computed(() =>route.name);
+    const onClickLeft = () => history.back();
+    const onClickRight = () => Toast('按钮');
     return {
-      pathName: '',
+      pathName,
+      onClickLeft,
+      onClickRight,
     };
   },
-  created () {
-    this.pathName = this.$route.name;
-  },
-  methods: {
-    onClickLeft () {
-      this.$router.go(-1);
-    },
-    onClickRight () {
-      Toast('按钮');
-    },
-  },
+  // data () {
+  //   return {
+  //     pathName: '',
+  //   };
+  // },
+  // created () {
+  //   this.pathName = this.$route.name;
+  // },
+  // methods: {
+  //   onClickLeft () {
+  //     this.$router.go(-1);
+  //   },
+  //   onClickRight () {
+  //     Toast('按钮');
+  //   },
+  // },
 }
 </script>
